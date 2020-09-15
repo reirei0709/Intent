2ea348ff3cc0bcad466ed5d6711f6dc67b78ba53
mirei <usrmii301@gmail.com>
2020-09-15 20:12:06 +0900

    写真が表示されない(ギャラリービュー)

---
+++ app/src/main/java/app/itakura/reirei/intent/SecondActivity.kt
@@ -0,0 +1,13 @@
+package app.itakura.reirei.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+    }
+}
